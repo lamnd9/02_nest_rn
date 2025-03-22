b372dba0bba12c0841b02035e0e48db416a5e68b
Nguyen Duy Lam <lamnd9@gmail.com>
2025-03-22 07:44:55 +0700

    curd

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,11 @@
-export class CreateUserDto {}
+import { IsNotEmpty } from "class-validator"
+export class CreateUserDto {
+    @IsNotEmpty()
+    name: string;
+    email: string;
+    password: string;
+    phone: string;
+    address: string;
+    image: string;
+}
+
